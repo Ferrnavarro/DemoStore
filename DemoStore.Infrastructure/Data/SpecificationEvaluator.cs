b493f1e77abcdd3721b5dd7ac42315cf8c19e717
ferrnavarro <fer.maganavarro@gmail.com>
2020-05-01 13:05:40 -0600

    Add data repository

---
+++ DemoStore.Infrastructure/Data/SpecificationEvaluator.cs
@@ -0,0 +1,45 @@
+﻿using DemoStore.Core.Entities;
+using DemoStore.Core.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DemoStore.Infrastructure.Data
+{
+    public class SpecificationEvaluator<T> where T : BaseEntity
+    {
+        public static IQueryable<T> GetQuery(IQueryable<T> inputQuery, ISpecification<T> specification)
+        {
+            var query = inputQuery;
+       
+            if (specification.Criteria != null)
+            {
+                query = query.Where(specification.Criteria);
+            }
+     
+            query = specification.Includes.Aggregate(query,
+                                    (current, include) => current.Include(include));
+       
+            query = specification.IncludeStrings.Aggregate(query,
+                                    (current, include) => current.Include(include));
+      
+            if (specification.OrderBy != null)
+            {
+                query = query.OrderBy(specification.OrderBy);
+            }
+            else if (specification.OrderByDescending != null)
+            {
+                query = query.OrderByDescending(specification.OrderByDescending);
+            }         
+     
+            if (specification.IsPagingEnabled)
+            {
+                query = query.Skip(specification.Skip)
+                             .Take(specification.Take);
+            }
+            return query;
+        }
+    }
+}
