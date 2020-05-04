cbc88fe1a44c2a0b58897f463221b68f8377bf33
ferrnavarro <fer.maganavarro@gmail.com>
2020-05-04 16:44:44 -0600

    Add seed for demo data

--- DemoStore.API/Program.cs
+++ DemoStore.API/Program.cs
@@ -2,8 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DemoStore.Core.Entities.UserAggregate;
+using DemoStore.Infrastructure.Data;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -11,9 +15,29 @@ namespace DemoStore.API
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public async static Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<ApplicationDbContext>();
+                    await ApplicationDbContextSeed.SeedAsync(context);
+
+                    var userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
+                    var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+                    await ApplicationDbContextSeed.SeedUserAsync(userManager, roleManager);
+
+                }
+                catch (Exception ex )
+                {
+                    ex.ToString();
+                }
+            }
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
---
+++ DemoStore.Infrastructure/Data/ApplicationDbContextSeed.cs
@@ -0,0 +1,80 @@
+﻿using DemoStore.Core.Entities.ProductAggregate;
+using DemoStore.Core.Entities.UserAggregate;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoStore.Infrastructure.Data
+{
+    public class ApplicationDbContextSeed
+    {
+        public static async Task SeedAsync(ApplicationDbContext context)
+        {
+            if (!context.Products.Any())
+            {
+                context.Products.AddRange(GetPreconfiguredProducts());
+                await context.SaveChangesAsync();
+            }
+        }
+
+        private static IEnumerable<Product> GetPreconfiguredProducts()
+        {
+            return new List<Product>()
+            {
+                new Product()
+                {
+                    Name = "Nintendo Switch",
+                    Description = "Get the gaming system that lets you play the games you want, wherever you are, however you like. Includes the Nintendo Switch console and Nintendo Switch dock in black",
+                    NumberAvailable = 7,
+                    Price = 350,
+                    Sku = "NIN-SW-BLACK-82"
+                },
+
+                new Product()
+                {
+                    Name = "PlayStation 4",
+                    Description = "The official home of PlayStation 4 - the world's best-selling console. Available in Jet Black, Glacier White and limited edition Gold and Silver.",
+                    NumberAvailable = 7,
+                    Price = 450,
+                    Sku = "PLAY-ST-BLACK-82"
+                },
+
+                new Product()
+                {
+                    Name = "Xbox One",
+                    Description = "Take your gaming skills to a whole new level with Xbox gaming consoles and accessories",
+                    NumberAvailable = 7,
+                    Price = 400,
+                    Sku = "XB-ONE-WHITE-82"
+                },
+
+                new Product()
+                {
+                    Name = "Nintendo Switch",
+                    Description = "Get the gaming system that lets you play the games you want, wherever you are, however you like. Includes the Nintendo Switch console and Nintendo Switch dock in black",
+                    NumberAvailable = 7,
+                    Price = 350,
+                    Sku = "NIN-SW-BLACK-82"
+                }
+                
+            };
+        }
+
+        public static async Task SeedUserAsync(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            var defaultUser = new ApplicationUser
+            {
+                UserName = "demouser",
+                Email = "demouser@demo.com",
+                Name = "Demo User",
+                BirthDate = DateTime.Today,
+            };
+
+            await userManager.CreateAsync(defaultUser, "P@ssword123");           
+           
+        }
+    }
+}
